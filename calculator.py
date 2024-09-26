26afc5ca4584d6f8657a3fcc8d04d61e2a7e7cfd
DemahSultan <demah20122000@gmail.com>
2024-09-26 23:36:42 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,3 +24,13 @@ def power(x, y):
 
 def sqrt(x):
     return x**x 
+
+
+# For the add() function
+$ py.test test_add.py
+
+# For the subtract() function
+$ py.test test_subtract.py
+
+# For the mulitply() function
+$ py.test test_multiply.py
